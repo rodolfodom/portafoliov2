66490bacd6a3e70d948d8f58274fc7fc49a6dd14
rodolfodom <rodolfodominguez60@aragon.unam.mx>
2021-01-21 07:57:29 -0600

    I finished slider on project modal window

--- js/modules/projectModal.js
+++ js/modules/projectModal.js
@@ -5,8 +5,10 @@ export async function projectModal() {
   let $projectsCards = d.querySelectorAll(".project-card aside"),
     $modalProject = d.querySelector(".project-modal"),
     slideActive = 0,
-    $slidesFragment = d.createDocumentFragment();
+    $slidesFragment = d.createDocumentFragment(),
+    slidesList;
 
+  // asignar evento a todas la tarjetas para generar el contenido de la ventana modal.
   for (let i = 0; i < $projectsCards.length; i++) {
     $projectsCards[i].addEventListener("click", (e) => {
       let index = $projectsCards[i].dataset.index;
@@ -18,9 +20,10 @@ export async function projectModal() {
         $slide.appendChild($img);
         $slidesFragment.appendChild($slide);
       });
+      //generación de la descripción.
       $modalProject.querySelector(".project-modal-info").innerHTML = `
       <h4>${PROJECTS[index].name}</h4><br>
-      <p>${PROJECTS[index].longDescription}.</p>
+      <p>${PROJECTS[index].longDescription}</p>
       <ul>
       <li><b>Tipo de proyecto:</b> ${PROJECTS[index].type}.</li>
       <li><b>Tecnologías:</b> ${PROJECTS[index].technologies}.</li>
@@ -40,39 +43,41 @@ export async function projectModal() {
         PROJECTS[index].pageLink ? PROJECTS[index].pageLink : ""
       }" target="_blank">${PROJECTS[index].pageLink ? "Ir al proyecto" : ""}</a>
       `;
-      $modalProject.querySelector(".slides").innerHTML = "";
       $modalProject.querySelector(".slides").appendChild($slidesFragment);
       $modalProject
         .querySelector(".slides")
-        .children[1].classList.add("active");
+        .children[slideActive].classList.add("active");
+      slidesList = d.querySelectorAll(".slide");
       $modalProject.classList.remove("hidden");
     });
   }
 
   d.addEventListener("click", (e) => {
+    //botón de cierre de la ventana modal.
     if (
       e.target.matches("#close-modal") ||
       e.target.matches("#close-modal *")
     ) {
-      slideActive = 0;
       $modalProject.classList.add("hidden");
+      slideActive = 0;
+      $modalProject.querySelector(".slides").innerHTML = "";
     }
 
-    if (
-      e.target.matches("#prev") ||
-      e.target.matches("#next") ||
-      e.target.matches("#next *") ||
-      e.target.matches("#prev *")
-    ) {
-      let slidesList = d.querySelectorAll(".slide");
+    if (e.target.matches("#prev") || e.target.matches("#prev *")) {
       slidesList[slideActive].classList.remove("active");
-      if (e.target.matches("#prev") || e.target.matches("#prev *")) {
-        slideActive--;
-        if (slideActive < 0) slideActive = slidesList.length - 1;
-      } else if (e.target.matches("#next") || e.target.matches("#next *")) {
-        slideActive++;
-        if (slideActive > slidesList.length - 1) slideActive = 0;
-      }
+      slideActive--;
+      if (slideActive < 0) slideActive = slidesList.length - 1;
+
+      slidesList[slideActive].classList.add("active");
+    }
+
+    if (e.target.matches("#next") || e.target.matches("#next *")) {
+      slidesList[slideActive].classList.remove("active");
+
+      slideActive++;
+
+      if (slideActive > slidesList.length - 1) slideActive = 0;
+
       slidesList[slideActive].classList.add("active");
     }
   });
